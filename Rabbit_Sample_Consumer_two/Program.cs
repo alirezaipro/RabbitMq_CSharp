5988449f828a0092603addf9ca7b20341a6c8ac9
iman <alirezai.pro@gmail.com>
2024-11-05 23:43:07 +0330

    feat: add project files

---
+++ Rabbit_Sample_Consumer_two/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Consumer_Two");
+Console.WriteLine("-------------------");
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+using var connection = connectionFactory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+string exchangeName = "alirezaei_exchange_direct";
+channel.ExchangeDeclare(exchange: exchangeName, ExchangeType.Fanout);
+
+
+string queueName = channel.QueueDeclare().QueueName;
+//string routingKey = "log.error";
+
+channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: "");
+
+var consumerEvent = new EventingBasicConsumer(channel);
+
+consumerEvent.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"message = {message}");
+};
+
+channel.BasicConsume(queueName, true, consumerEvent);
+
+Console.WriteLine("Consumer finish");
+
+Console.ReadLine();
---
+++ Rabbit_Sample_Producer/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var connectionFactory=new ConnectionFactory()
+{
+    HostName="localhost"
+};
+
+using var connection = connectionFactory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+string exchangeName = "alirezaei_exchange_direct";
+channel.ExchangeDeclare(exchange: exchangeName, ExchangeType.Fanout);
+
+bool isRepeat = true;
+
+while (isRepeat)
+{
+    Console.WriteLine("please enter command");
+    string command = Console.ReadLine();
+
+    switch (command)
+    {
+        case "p":
+
+            //Console.WriteLine("Please enter routing key");
+            //string routingKey = Console.ReadLine();
+
+            Console.WriteLine("Please enter message");
+            string message = Console.ReadLine();
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: exchangeName, routingKey: "", basicProperties: null, body: body);
+
+            Console.WriteLine("-----------------------");
+
+            break;
+
+
+        default:
+        case "e":
+            isRepeat = false;
+            break;
+    }
+}
+
+Console.ReadLine();
